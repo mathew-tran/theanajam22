43de3e4172d3078db9f40bb0491abfca780b7b5f
mathew-tran <mathew.tran.dev@gmail.com>
2024-06-04 18:17:29 -0700

    have rough draft of player controller

--- Scripts/Game/PlayerController.gd
+++ Scripts/Game/PlayerController.gd
@@ -4,22 +4,30 @@ var HorizontalSpeed = 200
 var VerticalSpeed = 200
 
 var VerticalRestraints = {
-	"MIN" : 376,
-	"MAX" : 800
+	"MIN" : -3000,
+	"MAX" : 500
 }
 
 var HorizontalRestraints = {
-	"MIN" : 376,
-	"MAX" : 800
+	"MIN" : 0,
+	"MAX" : 3000
 }
 
 func _process(delta):
 	if Input.is_action_pressed("move_left"):
 		global_position.x -= HorizontalSpeed * delta
+		if global_position.x <= HorizontalRestraints["MIN"]:
+			global_position.x = HorizontalRestraints["MIN"]
 	if Input.is_action_pressed("move_right"):
 		global_position.x += HorizontalSpeed * delta
+		if global_position.x > HorizontalRestraints["MAX"]:
+			global_position.x = HorizontalRestraints["MAX"]
 
 	if Input.is_action_pressed("move_up"):
 		global_position.y -= VerticalSpeed * delta
+		if global_position.y <= VerticalRestraints["MIN"]:
+			global_position.y = VerticalRestraints["MIN"]
 	if Input.is_action_pressed("move_down"):
 		global_position.y += VerticalSpeed * delta
+		if global_position.y > VerticalRestraints["MAX"]:
+			global_position.y = VerticalRestraints["MAX"]
